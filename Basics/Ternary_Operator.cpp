22c5809d35eca597b8f3b9f655d69a5330263e4d
Hira <HiraShahid2006@gmail.com>
2026-03-30 14:27:06 +0100

    Working on

---
+++ Basics/Ternary_Operator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+    //ternary operator ?: = a replacement to an if statement
+    // condition ? expression1 : expression2
+
+    /* my attempt at using a ternary operator without watching the video
+
+    int grade = 70;
+
+    grade == 70 ? ("YOU PASS!") : ("YOU DID NOT PASS"); 
+
+    std::cout << grade; //only prints the grade number, doesnt print the text statements
+
+    https://www.w3schools.com/cpp/cpp_conditions_shorthand.asp
+    */
+
+
+
+    return 0;
+}
