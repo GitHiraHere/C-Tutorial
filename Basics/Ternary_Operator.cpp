274c297d48628403c66c4e3818d0a0ece0c64d86
Hira <HiraShahid2006@gmail.com>
2026-03-30 19:41:16 +0100

    Completed

--- Basics/Ternary_Operator.cpp
+++ Basics/Ternary_Operator.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 
 int main(){
-    //ternary operator ?: = a replacement to an if statement
-    // condition ? expression1 : expression2
+    /*ternary operator ?: = a replacement to an if statement
+    / variable = (condition) ? expressionTrue : expressionFalse;
 
-    /* my attempt at using a ternary operator without watching the video
-
-    int grade = 70;
-
-    grade == 70 ? ("YOU PASS!") : ("YOU DID NOT PASS"); 
-
-    std::cout << grade; //only prints the grade number, doesnt print the text statements
-
-    https://www.w3schools.com/cpp/cpp_conditions_shorthand.asp
+     my attempt at using a ternary operator without watching the video
+     https://www.w3schools.com/cpp/cpp_conditions_shorthand.asp
     */
+    int grade = 70;
+    std::string result;
 
+    result = (grade == 70) ? "YOU PASS!\n" : "YOU DID NOT PASS\n"; //brackets are optional  
 
+    //bro codes version
+
+    //<< insertion operator, sends data from an output stream
+    // >> extraction operator, takes data from an input stream
+    grade >= 60 ? std::cout << "You pass!" << "\n" : std::cout << "You fail!" << "\n"; //this does not get stored into a variable
+
+    std::cout << result;
+
+    bool hungry = true;
+
+    //when checking a bool you dont need to do bool variable == true as variable ? does the same similar to if(variable)
+    hungry ? std::cout << "You are hungry!" : std::cout << "You are not hungry";
 
     return 0;
 }
